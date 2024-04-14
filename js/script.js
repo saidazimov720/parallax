7872f1994f0482836a5095094c1ace16e08226c1
saidazimov720 <saidazimovnurbek01gmail.com>
2024-04-14 12:48:19 +0500

    plugin

--- js/script.js
+++ js/script.js
@@ -2,3 +2,9 @@ window.addEventListener('scroll', e =>{
     document.documentElement.style.setProperty(
         '--scrollTop', `${this.screenY}px`)
 })
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
+ 
